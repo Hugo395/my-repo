91e2903d8a99baa6d7470579f61e971245da284c
Andre R <andre.rabaca94@gmail.com>
2020-05-05 17:09:23 +0100

    Adding dropdownlist failed , not possible only with JQuery, added password strenght

--- trivia-game/routes/auth-routes.js
+++ trivia-game/routes/auth-routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
-const bcryptSalt = 10 
+const bcryptSalt = 10
+const  zxcvbn = require('zxcvbn');
+zxcvbn('Tr0ub4dour&3');
 
 router.get("/sign-up", (req, res, next) => {
   try {
@@ -15,23 +17,34 @@ router.get("/sign-up", (req, res, next) => {
 router.post("/sign-up", (req,res,next)=>{
   const username = req.body.username
   const password = req.body.password
+  const passwordStrengh = zxcvbn(password)  
+
   const salt = bcrypt.genSaltSync(bcryptSalt)
   const hashPass = bcrypt.hashSync(password,salt)
   //const isPassword = password.match(/[A-Z]/g)
   //if(!isPasswordOk){}
 
   //Making sure username and password are not empty
-
+  console.log(passwordStrengh)
   if(username === "" || password === ""){
     res.render("auth/sign-up", {
       errorMessage: "Indicate a username and password"
     });
     return;
   }
+  //Password strengh check
+  if(passwordStrengh.score<3){
+    res.render("auth/sign-up", {
+      errorMessage : "Your password is too weak",
+      warning: passwordStrengh.feedback.warning,
+      suggestion : passwordStrengh.feedback.suggestions[0]
+    });
+  }
+  
+
   //Making sure that user doens't exist already
 User.findOne({"username":username})
   .then(user => {
-    console.log(user)
     if(user!==null){
       res.render("auth/sign-up",{
       errorMessage: "The username already exists"
@@ -50,7 +63,6 @@ User.findOne({"username":username})
 })
   
   })
-
   
   router.get("/login",(req,res,next)=>{
     try{
